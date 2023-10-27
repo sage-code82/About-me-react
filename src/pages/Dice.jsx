8ce01ec9f35a261463cccdde6de815b130d0f715
Stephen <sagelvl40@gmail.com>
2023-10-27 15:58:53 +0100

    start the dice rolling function d6 to start with

--- src/pages/Dice.jsx
+++ src/pages/Dice.jsx
@@ -1,8 +1,21 @@
-export default function About() {
+import React, { useState } from "react";
+
+export default function Dice() {
+  const [numberOfDice, setNumberOfDice] = useState(1);
+  const [rollResult, setRollResult] = useState(null);
+
+  const rollDice = () => {
+    const maxRoll = 6;
+    let totalRoll = 0;
+    for (let i = 0; i < numberOfDice; i++) {
+      totalRoll += Math.floor(Math.random() * maxRoll) + 1;
+    }
+    setRollResult(totalRoll);
+  };
+
   return (
     <main>
-      <h2>Dice</h2>
-      <p>This will be my dice page</p>
+      <h2>Chuck Some Dice</h2>
     </main>
   );
 }
