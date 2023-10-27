e32d16917ee764af897487e82c9a8c944d250066
Stephen <sagelvl40@gmail.com>
2023-10-27 14:33:35 +0100

    set up the pages, install router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Dice from "./pages/Dice";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>ALL ABOUT ME</h1>
+        <Link to="/">Home</Link>
+        <Link to="/dice">Dice</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="dice" element={<Dice />} />
+      </Routes>
+
+      <footer>
+        <p>&copy; Stephen Sage 41 and 1 quarter</p>
+      </footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Dice.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h2>Dice</h2>
+      <p>This will be my dice page</p>
+    </main>
+  );
+}
